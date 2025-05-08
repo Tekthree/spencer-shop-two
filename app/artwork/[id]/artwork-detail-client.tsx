4ecb25764f5fe1d593880c8c8e689a8e86c07b5b
tekthree <tekthree@gmail.com>
2025-05-08 15:47:49 -0700

    feat: create artwork detail page with responsive gallery and sticky product info

--- app/artwork/[id]/artwork-detail-client.tsx
+++ app/artwork/[id]/artwork-detail-client.tsx
@@ -403,8 +403,8 @@ export default function ArtworkDetailClient({
             {/* Shipping Information Icons */}
             <div className="grid grid-cols-4 gap-2 text-center py-4 border-t border-gray-200 mb-6">
               <div className="flex flex-col items-center">
-                <div className="bg-gray-100 p-2 rounded-full mb-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="bg-[#0070ba]/10 p-2 rounded-full mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#0070ba]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
                   </svg>
                 </div>
@@ -413,8 +413,8 @@ export default function ArtworkDetailClient({
               </div>
               
               <div className="flex flex-col items-center">
-                <div className="bg-gray-100 p-2 rounded-full mb-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="bg-[#0070ba]/10 p-2 rounded-full mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#0070ba]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
                   </svg>
                 </div>
@@ -423,8 +423,8 @@ export default function ArtworkDetailClient({
               </div>
               
               <div className="flex flex-col items-center">
-                <div className="bg-gray-100 p-2 rounded-full mb-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="bg-[#0070ba]/10 p-2 rounded-full mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#0070ba]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </div>
@@ -433,8 +433,8 @@ export default function ArtworkDetailClient({
               </div>
               
               <div className="flex flex-col items-center">
-                <div className="bg-gray-100 p-2 rounded-full mb-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="bg-[#0070ba]/10 p-2 rounded-full mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#0070ba]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </div>
